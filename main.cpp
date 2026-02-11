c97bda6e41966aa1a44f3fae94a94b4aea3c8a8a
Nataso08 <elia.maso18@gmail.com>
2026-02-11 22:07:38 +0100

    Tried to improve a lightest resolution method (failed at the moment) :(

--- main.cpp
+++ main.cpp
@@ -1,48 +1,98 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <math.h>
 #include <algorithm>
-#include "solve.cpp"
 
 using namespace std;
 
+short N;
+vector<vector<short>> grid;
+
+struct findColumn {
+    short c;
+    short n;
+
+    findColumn (short c, short n) : c(c), n(n) {}
+
+    bool operator ()(const vector<short>& row) const {
+        return row[c] == n;
+    }
+};
+
+bool checkPossible (short r, short c, short n);
+void solve (int i0, int j0);
+void print ();
+
+
 int main () {
-    int size;
-    cout << "Insert the sudoku dimension: >>> ";
-    cin >> size;
+    // setup griglia
+    cin >> N;
+    grid.resize(N);
+    for (short i=0; i<N; i++) grid[i].resize(N);
+    
+    // input sudoku
+    for (short i=0; i<N; i++) {
+        for (short j=0; j<N; j++) 
+            cin >> grid[i][j];
+    }
+    cout << endl;
 
-    vector<vector<int>> sudoku_input (size, vector<int> (size, 0));
+    solve(0, 0);
 
-    int empty_cells = 0;
+    print();
+}
 
-    for (int i=0; i<size; i++) {
-        for (int j=0; j<size; j++) {
-            cin >> sudoku_input[i][j];
 
-            if (sudoku_input[i][j] == 0) empty_cells ++;
+
+bool checkPossible (short r, short c, short n) {
+    // 1) check row
+    if (find(grid[r].begin(), grid[r].end(), n) != grid[r].end()) return 0;
+
+
+    // 2) check column
+    if (find_if(grid.begin(), grid.end(), findColumn(c, n)) != grid.end()) return 0;
+
+
+    // 3) check square
+    short l = sqrt(N);
+    
+    // cella origine quadrato
+    short r0 = (r/l) *l;
+    short c0 = (c/l) *l;
+
+    for (int i=r0; i<r0+l; i++) {
+        for (int j=c0; j<c0+l; j++) 
+            if (grid[i][j] == n) return 0;
+    }
+
+    return 1;
+}
+
+void solve (int i0, int j0) {
+    print();
+    for (int i=i0; i<N; i++) {
+        for (int j=j0; j<N; j++) {
+            if (grid[i][j] == 0) {
+                for (int n=1; n<=N; n++) {
+                    if (checkPossible(i, j, n)) {
+                        grid[i][j] = n;
+                        solve (0, 0);
+                        grid[i][j] = 0;
+                        print();
+                    }
+                }
+                return ;
+            }
         }
     }
+}
 
-    if (!check(size, sudoku_input)) {
-        cout << "The inserted sudoku is unsolvable" << endl;
-        return 0;
+void print () {
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) 
+            cout << grid[i][j] << ((j+1)%(int)sqrt(N) == 0 ? "  " : " ");
+        cout << ((i+1)%(int)sqrt(N) == 0 ? "\n" : "") << endl;
     }
-
-    // regular_solve (size, sudoku_input, empty_cells);
-    
-    vector<vector<int>> sudoku = sudoku_input;
-    
-    int solved_cells = 0;
-
-    brute_force_solve(size, sudoku, empty_cells);
-
-    system("cls");
-    // cout << regular_solve (size, sudoku, empty_cells) << endl;
-    // print_sudoku(size, sudoku);
-
-    print_sudoku (size, sudoku);
-
-    system("pause");
-
-    return 0;
+    cout << endl << endl;
 }
